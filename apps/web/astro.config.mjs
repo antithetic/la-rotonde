7b86d68a8dae499f41a8c0b3716513d87f3f2815
Raúl Garcia <ego.war@icloud.com>
2026-02-20 20:06:27 -0800

    🎨 Nitpick
    nitt pick

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
-import UnoCSS from 'unocss/astro'
+
 
 // https://astro.build/config
 export default defineConfig({
