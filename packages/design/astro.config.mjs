fafddae4a0b871a6d4525f89cb68ad1a1f156b19
Raúl Garcia <ego.war@icloud.com>
2026-02-20 23:14:56 -0800

    ✨ Design System Basic Implementation
    Design system basic implementation will look at hot reloading when packages change as well.

---
+++ packages/design/astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
+import sharedConfig from '@repo/unocss'
+
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [UnoCSS(sharedConfig)],
+})
