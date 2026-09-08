48b62a71a72db2b5139d918c0e52bd812e2e49e3
Raúl Garcia <ego.war@icloud.com>
2026-09-08 03:13:54 -0700

    ✨ Astro: Sanity integration config, tsconfig update
    Set up Sanity integration in Astro. Update the TypeScript config to remove the base path from the compiler options and update the new paths pattern.

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from '@astrojs/vercel'
 import UnoCSS from 'unocss/astro'
 
 import react from '@astrojs/react'
+import sanity from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,14 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
 
-  integrations: [UnoCSS(), react()],
+  integrations: [
+    UnoCSS(),
+    react(),
+    sanity({
+      projectId: 'kzqf9i5y',
+      dataset: 'development',
+      useCdn: false,
+      apiVersion: '2026-09-01',
+    }),
+  ],
 })
