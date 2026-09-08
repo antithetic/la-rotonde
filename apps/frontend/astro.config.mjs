cb4ba727582d078a8b6aecfe4014582f618eba4a
Raúl Garcia <ego.war@icloud.com>
2026-09-08 00:03:14 -0700

    ➕ Astro: sanity + react integration
    Added Astro integrations for Sanity and React to start building out the frontend.

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -5,11 +5,13 @@ import { unoConfig } from '@repo/unocss'
 import vercel from '@astrojs/vercel'
 import UnoCSS from 'unocss/astro'
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://larotonde.cafe',
   output: 'server',
   adapter: vercel(),
 
-  integrations: [UnoCSS(unoConfig)],
+  integrations: [UnoCSS(unoConfig), react()],
 })
