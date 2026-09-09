78e85cbc594fbe5d4cbaac74cc217a292d754037
Raúl Garcia <ego.war@icloud.com>
2026-09-09 05:17:47 -0700

    ✨ Feat(pagebuilder): PageBuilder, PortableText render and base blocks components
    Beginning of implementing these components for Page Builder and Astro

--- apps/frontend/src/lib/sanity/queries.ts
+++ apps/frontend/src/lib/sanity/queries.ts
@@ -1,1 +1,48 @@
-export const SITE_SETTINGS_QUERY = `*[_type == "siteSettings"][0]{ siteTitle, tagline, copyrightText }`
+import { defineQuery } from 'groq'
+
+export const SITE_SETTINGS_QUERY = defineQuery(
+  `*[_type == "siteSettings"][0]{ siteTitle, tagline, copyrightText }`,
+)
+
+export const HOME_PAGE_QUERY = defineQuery(`
+  *[_id == "siteSettings"][0].homePage->{
+    _id,
+    _type,
+    title,
+    slug,
+    seo,
+
+    content[]{
+      ...,
+
+      _type == "heroBlock" => {
+        _type,
+        _key,
+        title,
+        image {
+          ...,
+          alt
+        },
+        content
+      },
+
+      _type == "richTextBlock" => {
+        _type,
+        _key,
+        title,
+        content
+      },
+
+      _type == "faqs" => {
+        _type,
+        _key,
+        faqs[]->{
+          _id,
+          _type,
+          question,
+          answer
+        }
+      }
+    }
+  }
+`)
